3b639c4c9e0eaa0fc8227329ab6dbedf834cb645
Gabriel Tamanini <gabitamanini1437@gmail.com>
2025-08-26 16:36:46 -0300

    Implementando as classes de Soma e Subtração

---
+++ src/AppCalculadora.java
@@ -0,0 +1,10 @@
+public class AppCalculadora {
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtracao(int a, int b) {
+        return a - b;
+    }
+    
+}
